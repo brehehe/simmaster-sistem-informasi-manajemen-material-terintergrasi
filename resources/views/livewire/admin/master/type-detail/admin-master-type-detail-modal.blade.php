e226348b2ba32f27c46e3b6466c2e0b97a5c98e2
Ubuntu <ubuntu@localhost.localdomain>
2026-07-13 11:29:16 +0800

    update

--- resources/views/livewire/admin/master/type-detail/admin-master-type-detail-modal.blade.php
+++ resources/views/livewire/admin/master/type-detail/admin-master-type-detail-modal.blade.php
@@ -16,6 +16,16 @@
             </div>
             <form wire:submit="save" class="p-5 space-y-4">
                 <div>
+                    <label class="block text-sm font-medium text-gray-700 mb-1">Nama <span
+                            class="text-red-500">*</span></label>
+                    <input type="text" wire:model="name"
+                        class="w-full px-3 py-2 text-sm rounded-lg border {{ $errors->has('name') ? 'border-red-300' : 'border-gray-200' }} focus:border-blue-500 focus:ring-2 focus:ring-blue-500/20 bg-gray-50"
+                        placeholder="Nama">
+                    @error('name')
+                        <p class="mt-1 text-xs text-red-500">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div>
                     <label class="block text-sm font-medium text-gray-700 mb-1">Tipe <span
                             class="text-red-500">*</span></label>
                     <div wire:ignore wire:key="select-type-{{ $typeDetailId ?? 'new' }}">
