3fbf7160b3e04c078952746d9f97a9b1f82a5e87
brehehe <bratang1h@gmail.com>
2026-08-24 11:33:29 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(\Spatie\Permission\PermissionRegistrar::class);
     }
 
     /**
